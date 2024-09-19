03925b09fd390a4c5e76eba0114d55dd24b4d42b
Yash Verma <yashv1752@gmail.com>
2024-09-19 14:00:13 +0530

    some changes made to export

--- src/components/ExportButton.jsx
+++ src/components/ExportButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CSVLink } from 'react-csv';
 import { Button, Spinner } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
 const ExportButton = ({ selectedUsers, users, onExport, loading }) => {
   const selectedData = selectedUsers.map(id => ({ id: id, ...users.find(user => user.id === id) }));
@@ -16,7 +17,8 @@ const ExportButton = ({ selectedUsers, users, onExport, loading }) => {
           { label: 'ID', key: 'id' },
           { label: 'Email', key: 'email' },
           { label: 'First Name', key: 'firstName' },
-          { label: 'Last Name', key: 'lastName' }
+          { label: 'Last Name', key: 'lastName' },
+          { label: 'Password', key: 'password' }
         ]}
         filename="users.csv"
       />
@@ -24,4 +26,17 @@ const ExportButton = ({ selectedUsers, users, onExport, loading }) => {
   );
 };
 
+ExportButton.propTypes = {
+    selectedUsers: PropTypes.arrayOf(PropTypes.number).isRequired,
+    users: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      firstName: PropTypes.string.isRequired,
+      lastName: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired
+    })).isRequired,
+    onExport: PropTypes.func.isRequired,
+    loading: PropTypes.bool.isRequired
+};
+  
+  
 export default ExportButton;
